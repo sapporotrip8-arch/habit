8d778f7dab0d797ff97ede8703fb194ec372c02c
sapporotrip8-arch <sapporotrip8@gmail.com>
2026-02-23 01:56:03 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  // index.html がある場所をルートとして指定
+  root: "client",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
     },
   },
   build: {
-    outDir: "dist",
+    // ルートが client になるため、出力先を一つ上の層の dist に設定
+    outDir: "../dist",
     emptyOutDir: true,
   },
 });
